1a281bc51568393b7dbcf73d360ddb4e95b824a4
Yahahur <rodmaron_recinto@dlsl.edu.ph>
2024-03-18 10:13:05 +0800

    half

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ burgir.addEventListener("click", () =>{
     burgir.classList.toggle("open");  
     ulo.classList.toggle("open");  
 })
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.header');
+
+    window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+    });
+});
